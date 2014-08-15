c147d6793497fc7622211ac63810bdb68eaae606
machgo <fakE00r@gmail.com>
2014-08-16 00:19:24 +0200

    changes for new 1min-data, new layout (still ugly...), rain-graph added

--- public/javascripts/graphs.js
+++ public/javascripts/graphs.js
@@ -1,4 +1,4 @@
-function getGraphOptions(renderTo, name, data)
+function getGraphOptions(renderTo, name, yLabel, data)
 {
     var options = {
         chart: {
@@ -21,7 +21,7 @@ function getGraphOptions(renderTo, name, data)
         },
         yAxis: {
             title: {
-                text: name
+                text: yLabel
             }
         },
         plotOptions: {
@@ -54,6 +54,103 @@ function getGraphOptions(renderTo, name, data)
     return options;
 }
 
+function getWindspeedGraphOptions(renderTo, name1, name2, data1, data2)
+{
+    var options = {
+        chart: {
+            type: 'area',
+            renderTo: renderTo
+        },
+        title: {
+            text: name1
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: { // don't display the dummy year
+                month: '%e. %b',
+                year: '%b'
+            },
+            title: {
+                text: 'Date'
+            }
+
+        },
+        yAxis: {
+            title: {
+                text: "Speed (km/h)"
+            }
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    enabled: false
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+        series: [{
+            showInLegend: true,
+            name: name1,
+            data: data1
+        },
+        {
+            showInLegend: true,
+            name: name2,
+            data: data2
+        }
+        ]
+    };
+    return options;
+}
+
+function getRainGraphOptions(renderTo, name, data, labels)
+{
+    var options = {
+        chart: {
+            type: 'column',
+            renderTo: renderTo
+        },
+        title: {
+            text: 'Hourly Rainfall'
+        },
+        xAxis: {
+            categories: labels
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: "Rain (mm)"
+            }
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+            showInLegend: false,
+            name: 'Rain',
+            data: data
+        }]
+
+    };
+    return options;
+}
+
 
 $(function () {
     var tempcgraph;
@@ -62,14 +159,20 @@ $(function () {
     var winddirgraph;
     var windspeedkmhgraph;
     var batt_lvlgraph;
+    var light_lvlgraph;
+
+    var raingraph;
 
     $.get('/api/lastday', function(data){
-        tempcgraph = new Highcharts.Chart(getGraphOptions('tempcgraph', 'TempC', data.tempcvalues));
-        pressuregraph = new Highcharts.Chart(getGraphOptions('pressuregraph', 'Pressure', data.pressurevalues));
-        humiditygraph = new Highcharts.Chart(getGraphOptions('humiditygraph', 'Humidity', data.humidityvalues));
-        winddirgraph = new Highcharts.Chart(getGraphOptions('winddirgraph', 'Wind Direction', data.winddirvalues));
-        windspeedkmhgraph = new Highcharts.Chart(getGraphOptions('windspeedkmhgraph', 'Windspeed', data.windspeedkmhvalues));
-        batt_lvlgraph = new Highcharts.Chart(getGraphOptions('batt_lvlgraph', 'Battery', data.batt_lvlvalues));
+        tempcgraph = new Highcharts.Chart(getGraphOptions('tempcgraph', 'Temperature', "Temperature (Celsius)", data.tempcvalues));
+        pressuregraph = new Highcharts.Chart(getGraphOptions('pressuregraph', 'Barometer', "Pressure (Pa)", data.pressurevalues));
+        humiditygraph = new Highcharts.Chart(getGraphOptions('humiditygraph', 'Humidity', "Humidity (%)", data.humidityvalues));
+        winddirgraph = new Highcharts.Chart(getGraphOptions('winddirgraph', 'Wind Direction, "Direction (Degrees)"', data.winddirvalues));
+        windspeedkmhgraph = new Highcharts.Chart(getWindspeedGraphOptions('windspeedkmhgraph', 'Wind Speed', 'Gust Speed', data.windspeedkmhvalues, data.windgustspdvalues));
+        batt_lvlgraph = new Highcharts.Chart(getGraphOptions('batt_lvlgraph', 'Battery Voltage', "Voltage (V)", data.batt_lvlvalues));
+        light_lvlgraph = new Highcharts.Chart(getGraphOptions('light_lvlgraph', 'Light', "Light", data.light_lvlvalues));
+
+        raingraph = new Highcharts.Chart(getRainGraphOptions('raingraph', 'Rain (Hourly)', data.rainmm, data.rainlabels));
     });
 
 });
--- routes/api.js
+++ routes/api.js
@@ -28,17 +28,44 @@ exports.getLastDay = function(req, res){
 
     var query = db.collection('weatherdata').find({"timestamp": {$gte: yesterday, $lt: now}}).toArray(function (err, items) {
         var output = {pointStart: new Date(),tempcvalues: [], pressurevalues: [], humidityvalues: [],
-            windspeedkmhvalues: [], winddirvalues: [], batt_lvlvalues: [], light_lvlvalues: []};
+            windspdavgvalues: [], winddiravgvalues: [], windgustspdvalues: [], windgustdirvalues: [],
+            rainmm: [], rainlabels: [], batt_lvlvalues: [], light_lvlvalues: []};
         //
+        var rainmm = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+        var hour = new Date();
+        hour.setMilliseconds(0);
+        hour.setSeconds(0);
+        hour.setMinutes(0);
+
         for (var i=0; i<items.length; i++)
         {
-            output.tempcvalues.push([items[i].value.timestamp.getTime(), items[i].value.tempc]);
-            output.pressurevalues.push([items[i].value.timestamp.getTime(),items[i].value.pressure]);
-            output.humidityvalues.push([items[i].value.timestamp.getTime(),items[i].value.humidity]);
-            output.windspeedkmhvalues.push([items[i].value.timestamp.getTime(),items[i].value.windspeedkmh]);
-            output.winddirvalues.push([items[i].value.timestamp.getTime(),items[i].value.winddir]);
-            output.batt_lvlvalues.push([items[i].value.timestamp.getTime(),items[i].value.batt_lvl]);
-            output.light_lvlvalues.push([items[i].value.timestamp.getTime(),items[i].value.light_lvl]);
+            output.tempcvalues.push([items[i].timestamp.getTime(), items[i].tempc]);
+            output.pressurevalues.push([items[i].timestamp.getTime(),items[i].pressure]);
+            output.humidityvalues.push([items[i].timestamp.getTime(),items[i].humidity]);
+            output.windspdavgvalues.push([items[i].timestamp.getTime(),items[i].windspdavg]);
+            output.winddiravgvalues.push([items[i].timestamp.getTime(),items[i].winddiravg]);
+            output.windgustspdvalues.push([items[i].timestamp.getTime(),items[i].windgustspd]);
+            output.windgustdirvalues.push([items[i].timestamp.getTime(),items[i].windgustdir]);
+            output.batt_lvlvalues.push([items[i].timestamp.getTime(),items[i].batt_lvl]);
+            output.light_lvlvalues.push([items[i].timestamp.getTime(),items[i].light_lvl]);
+
+            var diff = items[i].timestamp;
+            diff.setMilliseconds(0);
+            diff.setSeconds(0);
+            diff.setMinutes(0);
+            diff = new Date(hour - diff);
+            var pos = 23 - (diff.getTime()/(60 * 60 * 1000));
+            rainmm[pos] += items[i].rainmm +1;
+        }
+
+        output.rainmm = rainmm;
+
+        for (var i=0; i<24; i++)
+        {
+            var hoursMinus = 23 - i;
+            var labelDate = new Date(hour.getTime()-(hoursMinus * 60 * 60 * 1000));
+            output.rainlabels.push(labelDate.getHours() + ":00");
         }
 
         res.json(output);
