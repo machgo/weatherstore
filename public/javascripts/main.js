716afbb7c80d216967a5e52d9308198bb6a4122b
machgo <marco.ms.schmid@gmail.com>
2014-08-22 21:50:17 +0200

    fixed winddirgraph

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,7 +13,7 @@ $(function () {
         tempcgraph = new Highcharts.Chart(getGraphOptions('tempcgraph', 'Temperature', "Temperature (Celsius)", data.tempcvalues));
         pressuregraph = new Highcharts.Chart(getGraphOptions('pressuregraph', 'Barometer', "Pressure (Pa)", data.pressurevalues));
         humiditygraph = new Highcharts.Chart(getGraphOptions('humiditygraph', 'Humidity', "Humidity (%)", data.humidityvalues));
-        winddirgraph = new Highcharts.Chart(getGraphOptions('winddirgraph', 'Wind Direction, "Direction (Degrees)"', data.winddirvalues));
+        winddirgraph = new Highcharts.Chart(getGraphOptions('winddirgraph', 'Wind Direction, "Direction (Degrees)"', data.winddiravgvalues));
         windspeedkmhgraph = new Highcharts.Chart(getWindspeedGraphOptions('windspeedkmhgraph', 'Wind Speed', 'Gust Speed', data.windspeedkmhvalues, data.windgustspdvalues));
         batt_lvlgraph = new Highcharts.Chart(getGraphOptions('batt_lvlgraph', 'Battery Voltage', "Voltage (V)", data.batt_lvlvalues));
         light_lvlgraph = new Highcharts.Chart(getGraphOptions('light_lvlgraph', 'Light', "Light", data.light_lvlvalues));
